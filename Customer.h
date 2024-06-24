287c06ad76dbeb20017a0810222d01d6bdeb12b8
Fajar Shakeel <127214002+manhalhads@users.noreply.github.com>
2024-06-24 16:44:55 +0500

    Update Customers.h

--- Customer.h
+++ Customer.h
@@ -15,25 +15,25 @@ protected:
   double saleslimit;
 
 public:
-  
+
   Customer(); //constructor
-  Customer(string CNIC, string name, string email, string phone, string type); //overloaded constructor
+  Customer(string CNIC, string name, string email, string phone, string type);
   string get_CNIC();
   string get_name();
-  string get_email();          //getters of Customer's attributes
+  string get_email();         
   string get_phone();
   string get_type();
   double get_saleslimit ();
   double get_amountpayable();
- 
+
   void set_CNIC(string cnic);
   void set_name(string n);
   void set_email(string e);
-  void set_phone(string p);           //setters of Customer's attributes
+  void set_phone(string p); 
   void set_type(string t);
   void set_saleslimit (string type);
   void set_amountpayable(double a);
-  
+
   void display();   //displays all attributes
   void add_new_customer();   // adds customer info to file
   void delete_customer();   //deletes customer info from file
